cefc32050e3f791d3a50647df60320a7c0152488
Sebastian Razola <700469+razola@users.noreply.github.com>
2018-09-13 15:34:01 +0200

    Update campaign.rb

--- lib/bing/ads/api/v11/data/campaign.rb
+++ lib/bing/ads/api/v11/data/campaign.rb
@@ -30,15 +30,11 @@ module Bing
 
             def self.prepare(campaign_raw)
               if campaign_raw[:bidding_scheme]
-                if campaign_raw[:bidding_scheme] == "TargetCpaBiddingScheme"
-                  campaign_raw[:bidding_scheme] = {
-                    type: campaign_raw[:bidding_scheme],
-                    '@xsi:type' => "#{Bing::Ads::API::V11::NAMESPACE_IDENTIFIER}:#{campaign_raw[:bidding_scheme]}",
-                    target_cpa: 5.0,
-                  }
+                # MaxClicksBiddingScheme, MaxConversionsBiddingScheme and TargetCpaBiddingScheme
+                if campaign_raw[:bidding_scheme].is_a?(Hash)
+                  campaign_raw[:bidding_scheme]['@xsi:type'] = "#{Bing::Ads::API::V11::NAMESPACE_IDENTIFIER}:#{campaign_raw[:bidding_scheme][:type]}"              
                 else
                   campaign_raw[:bidding_scheme] = {
-                    # TODO support MaxClicksBiddingScheme, MaxConversionsBiddingScheme and TargetCpaBiddingScheme
                     type: campaign_raw[:bidding_scheme],
                     '@xsi:type' => "#{Bing::Ads::API::V11::NAMESPACE_IDENTIFIER}:#{campaign_raw[:bidding_scheme]}"
                   }
